04668ddb9424b8c42bf06e1c982602adc371df1d
Berk Cambaz <berkcambaz12321@gmail.com>
2023-11-15 20:12:00 +0200

    Add habits and memories section to home route

--- web/src/routes/main/Home.tsx
+++ web/src/routes/main/Home.tsx
@@ -1,50 +1,81 @@
 import { Button, Flex, Paper, SimpleGrid, Text, Title } from "@mantine/core"
-import { IconFlame, IconRocket, IconStarFilled } from "@tabler/icons-react"
+import { IconFlame, IconMinus, IconPlus, IconRocket, IconStarFilled } from "@tabler/icons-react"
 
 function Home() {
   return (
-    <Flex direction="column" m="md" gap="md">
+    <Flex direction="column" m="md" gap="xl">
 
-      <Title order={4}>Welcome John!</Title>
+      <Flex direction="column" gap="xs">
 
-      <SimpleGrid cols={2} spacing="md">
+        <Title order={4}>Welcome John!</Title>
 
-        <Flex direction="column" align="start" gap="xs">
-          <Text>
-            Hello, world! Welcome to Trekie.
-            <Text fw="bold">The gamified digital life companion.</Text>
-          </Text>
+        <SimpleGrid cols={2} spacing="md">
 
-          <Button variant="filled">Primary action</Button>
-          <Button variant="default">Secondary action</Button>
-        </Flex>
+          <Flex direction="column" align="start" gap="xs">
+            <Text>
+              Hello, world! Welcome to Trekie.
+              <Text fw="bold">The gamified digital life companion.</Text>
+            </Text>
 
-        <Flex direction="column" gap="xs">
+            <Button variant="filled">Primary action</Button>
+            <Button variant="default">Secondary action</Button>
+          </Flex>
 
-          <Paper withBorder p="xs">
-            <Flex justify="center" gap="xs">
-              <IconRocket />
-              <Text>80%</Text>
-            </Flex>
-          </Paper>
+          <Flex direction="column" gap="xs">
 
-          <Paper withBorder p="xs">
-            <Flex justify="center" gap="xs">
-              <IconStarFilled />
-              <Text>964</Text>
-            </Flex>
-          </Paper>
+            <Paper withBorder p="xs">
+              <Flex justify="center" gap="xs">
+                <IconRocket />
+                <Text>80%</Text>
+              </Flex>
+            </Paper>
 
-          <Paper withBorder p="xs">
-            <Flex justify="center" gap="xs">
-              <IconFlame />
-              <Text>6 days</Text>
-            </Flex>
-          </Paper>
+            <Paper withBorder p="xs">
+              <Flex justify="center" gap="xs">
+                <IconStarFilled />
+                <Text>964</Text>
+              </Flex>
+            </Paper>
 
-        </Flex>
+            <Paper withBorder p="xs">
+              <Flex justify="center" gap="xs">
+                <IconFlame />
+                <Text>6 days</Text>
+              </Flex>
+            </Paper>
 
-      </SimpleGrid>
+          </Flex>
+
+        </SimpleGrid>
+
+      </Flex>
+
+      <Flex direction="column" gap="xs">
+
+        <Title order={4}>Habits</Title>
+
+        <Button.Group h={64}>
+          <Button h="auto">
+            <IconMinus />
+          </Button>
+          <Flex direction="column" justify="center" p="md" style={{ flex: 1 }}>
+            <Title order={5}>Write 15 pages of code</Title>
+            <Text>Gotta beat Linus Torvalds.</Text>
+          </Flex>
+          <Button h="auto">
+            <IconPlus />
+          </Button>
+        </Button.Group>
+
+      </Flex>
+
+      <Flex direction="column" gap="xs">
+
+        <Title order={4}>Memories</Title>
+
+        <Text>Can't remember right now. Please try again later...</Text>
+
+      </Flex>
 
     </Flex>
   )
