ebe4a1eea2f7ab715a2cc60fcf4b21f928480ece
Berk Cambaz <berkcambaz12321@gmail.com>
2023-11-15 19:55:43 +0200

    Add overview to the home route

--- web/src/routes/main/Home.tsx
+++ web/src/routes/main/Home.tsx
@@ -1,9 +1,51 @@
-import { Flex } from "@mantine/core"
+import { Button, Flex, Paper, SimpleGrid, Text, Title } from "@mantine/core"
+import { IconFlame, IconRocket, IconStarFilled } from "@tabler/icons-react"
 
 function Home() {
   return (
-    <Flex m="md">
-      Home
+    <Flex direction="column" m="md" gap="md">
+
+      <Title order={4}>Welcome John!</Title>
+
+      <SimpleGrid cols={2} spacing="md">
+
+        <Flex direction="column" align="start" gap="xs">
+          <Text>
+            Hello, world! Welcome to Trekie.
+            <Text fw="bold">The gamified digital life companion.</Text>
+          </Text>
+
+          <Button variant="filled">Primary action</Button>
+          <Button variant="default">Secondary action</Button>
+        </Flex>
+
+        <Flex direction="column" gap="xs">
+
+          <Paper withBorder p="xs">
+            <Flex justify="center" gap="xs">
+              <IconRocket />
+              <Text>80%</Text>
+            </Flex>
+          </Paper>
+
+          <Paper withBorder p="xs">
+            <Flex justify="center" gap="xs">
+              <IconStarFilled />
+              <Text>964</Text>
+            </Flex>
+          </Paper>
+
+          <Paper withBorder p="xs">
+            <Flex justify="center" gap="xs">
+              <IconFlame />
+              <Text>6 days</Text>
+            </Flex>
+          </Paper>
+
+        </Flex>
+
+      </SimpleGrid>
+
     </Flex>
   )
 }
