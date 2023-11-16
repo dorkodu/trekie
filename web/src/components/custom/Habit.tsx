28181ff4566701a06e5793f39e8e551b609fba11
Berk Cambaz <berkcambaz12321@gmail.com>
2023-11-16 13:12:22 +0200

    Add card background to habit

--- web/src/components/custom/Habit.tsx
+++ web/src/components/custom/Habit.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Text, Title } from "@mantine/core"
+import { Button, Card, Flex, Text, Title } from "@mantine/core"
 import { IconMinus, IconPlus } from "@tabler/icons-react"
 
 interface Props {
@@ -7,18 +7,20 @@ interface Props {
 
 function Habit({ }: Props) {
   return (
-    <Button.Group h={64}>
-      <Button h="auto">
-        <IconMinus />
-      </Button>
-      <Flex direction="column" justify="center" p="md" style={{ flex: 1 }}>
-        <Title order={5}>Write 15 pages of code</Title>
-        <Text>Gotta beat Linus Torvalds.</Text>
-      </Flex>
-      <Button h="auto">
-        <IconPlus />
-      </Button>
-    </Button.Group>
+    <Card withBorder p={0}>
+      <Button.Group h={64}>
+        <Button h="auto">
+          <IconMinus />
+        </Button>
+        <Flex direction="column" justify="center" p="md" style={{ flex: 1 }}>
+          <Title order={5}>Write 15 pages of code</Title>
+          <Text>Gotta beat Linus Torvalds.</Text>
+        </Flex>
+        <Button h="auto">
+          <IconPlus />
+        </Button>
+      </Button.Group>
+    </Card>
   )
 }
 
