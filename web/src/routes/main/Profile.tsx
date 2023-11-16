b09295ac719f0c2b3d47252ff1d6bd2bf775584c
Berk Cambaz <berkcambaz12321@gmail.com>
2023-11-16 13:08:55 +0200

    Update Profile.tsx

--- web/src/routes/main/Profile.tsx
+++ web/src/routes/main/Profile.tsx
@@ -2,10 +2,13 @@ import Community from "@/components/custom/Community"
 import Goal from "@/components/custom/Goal"
 import Memory from "@/components/custom/Memory"
 import ProfileMenu from "@/components/menus/ProfileMenu"
-import { Anchor, Avatar, Button, Card, Flex, Text, Title, rem } from "@mantine/core"
-import { IconChevronRight, IconDiscountCheckFilled } from "@tabler/icons-react"
+import { Anchor, Avatar, Button, Card, Flex, Paper, Text, Title, px, rem, useMantineTheme } from "@mantine/core"
+import { IconChevronRight, IconDiscountCheckFilled, IconFlame, IconRocket, IconStarFilled } from "@tabler/icons-react"
 
 function Profile() {
+  const theme = useMantineTheme();
+
+  console.log()
   return (
     <Flex direction="column" m="md">
 
@@ -20,40 +23,70 @@ function Profile() {
 
         <Flex direction="column" gap="md">
 
-          <Flex justify="space-between" gap="md" align="center">
+          <Avatar
+            src="https://avatars.githubusercontent.com/u/50113500?v=4"
+            radius={80}
+            size={80}
+            mt={-40 + (px(theme.spacing.xs) as number)}
+            style={{ border: `${rem(2)} solid var(--mantine-color-body)` }}
+            pos="absolute"
+          />
 
-            <Avatar
-              src="https://avatars.githubusercontent.com/u/50113500?v=4"
-              radius={80}
-              size={80}
-              mt={-30}
-              style={{ border: `${rem(2)} solid var(--mantine-color-body)` }}
-            />
-
-            <Flex align="center" gap="xs">
-              <ProfileMenu />
-              <Button radius="xl">Follow</Button>
-            </Flex>
-
+          <Flex align="center" justify="end" gap="xs" mt="xs">
+            <ProfileMenu />
+            <Button radius="xl">Follow</Button>
           </Flex>
 
-          <Flex direction="column">
+          <Flex direction="column" gap="xs">
 
-            <Flex align="center" gap="xs">
-              <Title order={5}>John Doe</Title>
-              <IconDiscountCheckFilled />
+            <Flex direction="column">
+              <Flex align="center">
+                <Title order={5}>John Doe</Title>
+                &nbsp;
+                <IconDiscountCheckFilled />
+              </Flex>
+              <Text>@johndoe</Text>
             </Flex>
-            <Text>@johndoe</Text>
 
-            <Text my="xs">Hello, world! This is my biography. I am John Doe.</Text>
+            <Text>Hello, world! This is my biography. I am John Doe.</Text>
 
             <Flex gap="xs">
               <Anchor>123 Followers</Anchor>
               <Anchor>123 Following</Anchor>
             </Flex>
 
-            <Flex>
-              test
+            <Flex direction="column" align="start" gap="xs">
+
+              <Paper withBorder p="md">
+                <Flex align="center" gap="xs">
+                  Momentum
+                  <Flex gap="xs">
+                    <IconRocket />
+                    <Text>80%</Text>
+                  </Flex>
+                </Flex>
+              </Paper>
+
+              <Paper withBorder p="md">
+                <Flex align="center" gap="xs">
+                  Experience
+                  <Flex gap="xs">
+                    <IconStarFilled />
+                    <Text>964</Text>
+                  </Flex>
+                </Flex>
+              </Paper>
+
+              <Paper withBorder p="md">
+                <Flex align="center" gap="xs">
+                  Streaks
+                  <Flex gap="xs">
+                    <IconFlame />
+                    <Text>6 days</Text>
+                  </Flex>
+                </Flex>
+              </Paper>
+
             </Flex>
 
           </Flex>
