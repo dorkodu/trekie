ea89ce73b9e3c363e4e1352eed10c0f4244ea9e1
Berk Cambaz <berkcambaz12321@gmail.com>
2023-11-17 11:36:00 +0200

    Update EditProfileModal.tsx

--- web/src/components/modals/EditProfileModal.tsx
+++ web/src/components/modals/EditProfileModal.tsx
@@ -1,9 +1,28 @@
+import { useApiStore } from "@/stores/apiStore";
 import { useAppStore } from "@/stores/appStore";
 import { Button, Flex, Modal, TextInput, Textarea } from "@mantine/core";
+import { useState } from "react";
 
 function EditProfileModal() {
   const editProfile = useAppStore(state => state.modals.editProfile);
   const close = () => useAppStore.setState(s => { s.modals.editProfile.opened = false })
+
+  const userId = useApiStore(state => state.userId);
+  const users = useApiStore(state => state.users);
+  const user = userId ? users[userId] : undefined;
+
+  const [name, setName] = useState(user?.name ?? "");
+  const [bio, setBio] = useState(user?.bio ?? "");
+
+  const confirm = () => {
+    useApiStore.setState(s => {
+      const currentUser = s.userId && s.users[s.userId];
+      if (!currentUser) return;
+      currentUser.name = name;
+      currentUser.bio = bio;
+    });
+    close();
+  }
 
   return (
     <Modal
@@ -17,19 +36,23 @@ function EditProfileModal() {
       <Flex direction="column" gap="md">
 
         <TextInput
-          label="Username"
-          placeholder="Username..."
+          label="Name"
+          placeholder="Name..."
+          value={name}
+          onChange={(ev) => setName(ev.currentTarget.value)}
         />
 
         <Textarea
           label="Bio"
           placeholder="Bio..."
+          value={bio}
+          onChange={(ev) => setBio(ev.currentTarget.value)}
           autosize
           minRows={3}
         />
 
         <Flex justify="end">
-          <Button>
+          <Button onClick={confirm}>
             Confirm
           </Button>
         </Flex>
