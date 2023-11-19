72208353cb9ce5e1415dc64959b3f973cfce948d
Berk Cambaz <berkcambaz12321@gmail.com>
2023-11-19 19:11:16 +0300

    Fix goal wrapping

--- web/src/components/custom/Goal.tsx
+++ web/src/components/custom/Goal.tsx
@@ -2,7 +2,7 @@ import { Badge, Flex, Paper, Text, Title } from "@mantine/core"
 import Emoji from "../Emoji"
 import { IGoal } from "@api/types/goal"
 import TextParser from "../util/TextParser";
-import { wrapContent } from "@/styles/shared.css";
+import { truncate } from "@/styles/shared.css";
 
 interface Props {
   goal: IGoal;
@@ -15,9 +15,9 @@ function Goal({ goal }: Props) {
 
         <Emoji emoji="👨‍💻" size={32} />
 
-        <Flex direction="column">
-          <Title order={5}><TextParser ids={["emoji"]} text={goal.title} /></Title>
-          <Text className={wrapContent}><TextParser ids={["emoji", "url", "username"]} text={goal.description} /></Text>
+        <Flex direction="column" style={{ display: "grid", gridTemplateColumns: "auto" }}>
+          <Title order={5} className={truncate}><TextParser ids={["emoji"]} text={goal.title} /></Title>
+          <Text truncate><TextParser ids={["emoji", "url", "username"]} text={goal.description} /></Text>
           <Flex mt="xs" gap="xs">
             <Badge>{goal.tasksDone} / {goal.tasksTodo} Tasks</Badge>
           </Flex>
