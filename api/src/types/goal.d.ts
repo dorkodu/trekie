bdf39ccebe7819dbc9b9a045529f634a351404c4
Doruk Eray <doruk@dorkodu.com>
2023-12-01 00:24:06 +0300

    (api) reorganize

--- api/src/types/goal.d.ts
+++
@@ -1,8 +0,0 @@
-export interface IGoal {
-  id: string;
-  userId: string;
-  title: string;
-  description: string;
-  tasksTodo: number;
-  tasksDone: number;
-}
--- api/src/types/memory.d.ts
+++
@@ -1,8 +0,0 @@
-export interface IMemory {
-  id: string;
-  userId: string;
-  date: number;
-  description: string;
-  favourites: number;
-  favourited?: boolean;
-}
