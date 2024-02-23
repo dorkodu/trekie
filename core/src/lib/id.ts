d23dce8886d52686f901a1950f78d0fdab5c2cd6
Doruk Eray <doruk@dorkodu.com>
2024-02-23 23:59:22 +0300

    lib: ID

--- core/src/lib/id.ts
+++ core/src/lib/id.ts
@@ -1,12 +1,27 @@
-import { randomBytes } from "crypto"
+import { sha256 } from '@noble/hashes/sha256';
+import { base58 } from '@scure/base';
 
-const uniq = () => crypto.subtle.digest("sha256", randomBytes(64))
+export function unique() {
+  const timestamp = Date.now().toString()
+  const random = crypto.getRandomValues(new Uint32Array(1)).toString()
+  // we mix timestamp with random bytes to increase uniqueness :)
+  const hash = sha256(`${timestamp}:${random}`)
 
-const ID = {
-  habit: () => (`trekie:habit:${uniq()}`),
-  user: () => (`trekie:user:${uniq()}`),
-  goal: () => (`trekie:goal:${uniq()}`),
-  story: () => (`trekie:story:${uniq()}`),
+  return hash.toString()
+}
+
+export const id = (namespace: string, label: string) => {
+  return base58.encode(
+    // Convert utf8 string to Uint8Array
+    new TextEncoder().encode(`${namespace}:${label}:${unique()}`)
+  )
+}
+
+export const ID = {
+  habit: () => id('trekie', 'habit'),
+  user: () => id('trekie', 'user'),
+  goal: () => id('trekie', 'goal'),
+  story: () => id('trekie', 'story'),
 }
 
 export default ID
