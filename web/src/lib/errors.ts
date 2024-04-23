4c7e87cb671953cda2644dc343c0dd5a9859f6a5
Doruk Eray <doruk@dorkodu.com>
2024-04-23 15:49:42 +0300

    error mgmt

---
+++ web/src/lib/errors.ts
@@ -0,0 +1,12 @@
+/**
+ *  We need to track results on function calls
+ *  If we encounter an error, function will fail and pass the error to this handler 
+ */
+
+
+/**
+ * 
+ */
+function handler(error: Error) {
+
+}
