dbbea1082697504991325f0736cdf7c576b31008
Doruk Eray <doruk@dorkodu.com>
2024-07-01 23:24:00 +0300

    add username regex

--- web/src/core/account.ts
+++ web/src/core/account.ts
@@ -1,4 +1,5 @@
 import { Timestamp } from "@/shared/utils"
+import { z } from "zod"
 
 export type IUser = IAccount & IProfile
 
@@ -28,3 +29,6 @@ export enum AccountTier {
   FREE = "free",
   PREMIUM = "premium",
 }
+
+export const USERNAME_REGEX = /^(?![_.])(?!.*[_.]{2})[a-zA-Z0-9_.]{1,16}(?<![_.])$/
+export const usernameSchema = z.string().trim().regex(USERNAME_REGEX)
