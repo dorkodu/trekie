5df259d1e7807f6531fc9a1c7c9dac0f7b14a137
Doruk Eray <doruk@dorkodu.com>
2024-09-30 02:20:50 +0300

    (core) create db for game

---
+++ web/src/core/db.ts
@@ -0,0 +1,23 @@
+import { ICommitRecord, ICommitResult, IUser } from '@/core'
+import Dexie, { Table } from 'dexie'
+
+export const db = new Dexie('trekie-game') as Dexie & {
+  commits: Table<ICommitRecord<any>, string>
+}
+
+// Schema declaration:
+db.version(1).stores({
+  commits: 'id, userId, timestamp'
+})
+
+db.on("populate", async () => { })
+
+db.on("ready", async () => {
+  console.info("db is ready")
+})
+
+db.open().then(async (db) => {
+  console.info("dexie opened successfully")
+}).catch((e) => {
+  console.error(e)
+})
