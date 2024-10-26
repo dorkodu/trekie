cd6440d1bdec818daa1c452ab2f93c9e55dcaf2e
Doruk Eray <doruk@dorkodu.com>
2024-10-26 11:39:35 +0300

    feat(web): [goal] get, giveup, commitments add/drop

--- web/src/namespaces/goal/index.ts
+++ web/src/namespaces/goal/index.ts
@@ -1,10 +1,11 @@
-import { CommitEvent, Commitment } from "@/core"
+import { CommitEvent, Commitment, ICommitmentInstance, ICommitmentKind } from "@/core"
 import { db } from "@/shared/lib/db"
 import trekie from '@/shared/lib/trekie'
-import { Maybe, Timestamp } from "@/shared/utils"
+import { arrayRemoveItem, Maybe, Timestamp } from "@/shared/utils"
 import { ulid } from "ulid"
 import { IGoal, IGoalTemplate, schema } from './schema'
 export * as Goal from "."
+export * from "./schema"
 export { schema } from './schema'
 
 export const commitment = Commitment('Goal', {
@@ -15,50 +16,128 @@ export const commitment = Commitment('Goal', {
   'PROGRESS_DONE': CommitEvent(() => ({ xp: +100, coins: 0 })),
   'COMMITMENT_ADD': CommitEvent(() => ({ xp: +1, coins: 0 })),
   'COMMITMENT_DROP': CommitEvent(() => ({ xp: -1, coins: 0 })),
-  '': CommitEvent(() => ({ xp: +1000, coins: +25 })),
+  'REACH': CommitEvent(() => ({ xp: +1000, coins: +25 })),
+  'GIVEUP': CommitEvent((status) => ({ xp: -100, coins: -1 })),
 })
 
 //? COMPONENT
 
 export interface Interface {
   get: (id: IGoal["id"]) => Promise<Maybe<IGoal>>
-  create: (template: IGoalTemplate) => Maybe<IGoal>
+  create: (template: IGoalTemplate) => Promise<Maybe<IGoal>>
   add: (goal: IGoal) => Promise<string>
-  update: (id: IGoal["id"], props: IGoalTemplate) => Maybe<IGoal>
+  update: (id: IGoal["id"], props: IGoalTemplate) => Promise<number>
   remove: (id: IGoal["id"]) => void
+  giveup: (id: IGoal["id"]) => void
   count: () => Promise<number>
+  addCommitment: (goalId: IGoal['id'], commitmentId: ICommitmentInstance['id']) => Promise<boolean>
+  dropCommitment: (goalId: IGoal['id'], commitmentId: ICommitmentInstance['id']) => Promise<boolean>
 }
 
 export const Component: Interface = {
   get: (id) => db.goals.get(id),
-  add: (goal) => {
-    return db.goals.add(goal, goal.id)
-  },
-  create(props) {
-    const userId = trekie.game().user?.id
-    if (!userId) return
-    let status = trekie.commitments.create('Habit')
+  add: (goal) => db.goals.add(goal, goal.id),
+  async create(props) {
+    let instance = trekie.commitments.create('Habit')
 
-    return {
+    let goal = {
       ...props,
       id: ulid(),
       xpCurrent: 0,
-      userId,
+      userId: trekie.game().user.id,
       createdAt: Date.now(),
       lastUpdated: Date.now(),
-      commitmentId: status.data.instance.id,
+      commitmentId: instance.id,
+      giveupAt: null,
+      completedAt: null
     } satisfies IGoal
-  },
-  update(id, props) {
-    const updatedGoal = this.create(props)
-    if (!updatedGoal) return
 
-    db.goals.update(id, props)
+    await this.add(goal)
 
-    return updatedGoal
+    trekie.commitments.act({
+      kind: 'Goal',
+      event: 'START',
+      id: goal.commitmentId,
+      data: { goalId: goal.id }
+    })
+
+    return goal
   },
+  async giveup(id) {
+    const goal = await db.goals.get(id)
+    if (!goal) return false
+
+    await db.goals.update(id, {
+      giveupAt: Date.now(),
+      lastUpdated: Date.now()
+    })
+
+    trekie.commitments.act({
+      kind: 'Goal',
+      event: 'GIVEUP',
+      id: goal.commitmentId,
+      data: { goalId: goal.id }
+    })
+
+    return true
+  },
+  async addCommitment(goalId, commitmentId) {
+    const goal = await this.get(goalId)
+    if (!goal) return false
+
+    const c = await trekie.commitments.get(commitmentId)
+    if (!c) return false
+
+    if (c.kind == 'Goal') return false // commitment can't be a goal 
+
+    goal.commitments.push(commitmentId)
+
+    await db.goals.update(goal.id, {
+      commitments: goal.commitments,
+      lastUpdated: Date.now()
+    })
+
+    trekie.commitments.act({
+      kind: 'Goal',
+      event: 'COMMITMENT_ADD',
+      id: commitmentId,
+      data: { goalId: goal.id, addedId: commitmentId }
+    })
+
+    return true
+  },
+  async dropCommitment(goalId, commitmentId) {
+    const goal = await db.goals.get(goalId)
+    if (!goal) return false
+
+    const cmt = await trekie.commitments.get(commitmentId)
+    if (!cmt) return false
+
+    if (cmt.kind == 'Goal') return false // commitment can't be a goal
+
+    await db.goals.update(goal.id, {
+      commitments: arrayRemoveItem(goal.commitments, commitmentId),
+      lastUpdated: Date.now()
+    })
+
+    trekie.commitments.act({
+      kind: 'Goal',
+      event: 'COMMITMENT_DROP',
+      id: commitmentId,
+      data: { goalId: goal.id, droppedId: commitmentId }
+    })
+
+    return true
+  },
+  update: (id, props) => db.goals.update(id, { ...props }),
   count: () => db.goals.count(),
-  remove: (id) => db.goals.delete(id),
+  remove: async (id) => {
+    let goal = await db.goals.get(id)
+    if (!goal) return
+
+    trekie.commitments.delete(goal.commitmentId)
+    await db.goals.delete(id)
+  }
 }
 
 export const goals = Component
