7c650b3e30d3dc81c5e93a12b5e42566d00177e0
Doruk Eray <doruk@dorkodu.com>
2024-11-02 10:22:53 +0300

    feat: account tiers and user schemas

--- web/src/core/account.ts
+++ web/src/core/account.ts
@@ -5,15 +5,17 @@ import { z } from "zod"
 export type IAccount = z.infer<typeof IAccount>
 export type IProfile = z.infer<typeof IProfile>
 export type IUser = IAccount & IProfile
+export enum AccountTier {
+  FREE = 0,
+  PREMIUM = 1,
+  BUSINESS = 2,
+  SUPERFAN = 3,
+}
 
 const IProfile = z.object({
   bio: z.string().trim().max(500),
-  birthDate: z.number(),
-  category: z
-  location: string
-  url?: string
-  
-  description: z.string().trim().min(1).max(500),
+  location: z.string().trim().max(100),
+  url: z.string().url().trim(),
 })
 
 const IAccount = z.object({
@@ -21,27 +23,13 @@ const IAccount = z.object({
   username: z.string().trim().regex(/^(?![_.])(?!.*[_.]{2})[a-zA-Z0-9_.]{1,16}(?<![_.])$/),
   name: z.string().trim().min(1).max(64),
   email: z.string().trim().email().max(320),
-
   pictureUrl: z.string().trim().url(),
-
   joinedAt: z.number(),
-
-  tier: z.enum({
-    FREE: 0,
-    PREMIUM: 1,
-    BUSINESS: 2,
-    SUPERFAN: 3,
-  }),
+  birthDate: z.number(),
+  tier: z.nativeEnum(AccountTier),
 })
 
 export const IUser = IAccount.merge(IProfile)
-
-export enum AccountTier {
-  FREE = 0,
-  PREMIUM = 1,
-  BUSINESS = 2,
-  SUPERFAN = 3,
-}
 
 export function createUser(input: {
   username: string
@@ -52,7 +40,7 @@ export function createUser(input: {
   category: string
   location: string
   url: string
-  birthday: number
+  birthDate: number
 }): IUser {
   return {
     id: ulid(),
@@ -64,13 +52,16 @@ export function createUser(input: {
     ✦ ENFJ • ♓ • 3w2 • E/Acc • Techno-optimist`,
     email: 'doruk@dorkodu.com',
     pictureUrl: '/images/doruk--green.png',
-    tier: 'PREMIUM',
-    category: "Entrepreneur",
+    tier: AccountTier.SUPERFAN,
     location: "Istanbul, TR",
     url: "https://doruk.dorkodu.com",
     joinedAt: new Date("19/02/2024 10:50").getTime(),
-    birthday: new Date("03/08/2004 09:45 AM").getTime(),
-  }
+    birthDate: new Date("03/08/2004 09:45 AM").getTime(),
+  } satisfies IUser
+}
+
+export function createAccount() {
+
 }
 
 
@@ -90,5 +81,5 @@ export const schema = {
     .refine((date) => /^\d{4}-\d{2}-\d{2}$/.test(date), {
       message: "Date must be in YYYY-MM-DD format.",
     })
-    .transform((date) => new Date(`${date}T00:00:00Z`)); // Convert to UTC date
+    .transform((date) => new Date(`${date}T00:00:00Z`)) // Convert to UTC date
 }
