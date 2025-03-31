286db11be71c657f87efc4e02cd4d1ac22aa7eae
Doruk Eray <doruk@dorkodu.com>
2025-03-31 23:10:21 +0300

    chore(api): remove /sync namespace

--- api/src/namespaces/sync/service.ts
+++
@@ -1,8 +0,0 @@
-import { IStatus } from "@sdk/utils/sync"
-
-export function receive(statuses: IStatus<any>) {
-
-}
-
-export * as syncService from "./service"
-
--- api/src/router.ts
+++ api/src/router.ts
@@ -2,14 +2,12 @@ import { Router } from "@api/lib/trpc"
 import { authEndpoints } from "./namespaces/auth/endpoints"
 import { gameEndpoints } from "./namespaces/game/endpoints"
 import { goalEndpoints } from "./namespaces/goal/endpoints"
-import { syncEndpoints } from "./namespaces/sync/endpoints"
 import { userEndpoints } from "./namespaces/user/endpoints"
 
 export const appRouter = Router({
   auth: authEndpoints.router,
   user: userEndpoints.router,
   game: gameEndpoints.router,
-  sync: syncEndpoints.router,
   goal: goalEndpoints.router,
 })
 
