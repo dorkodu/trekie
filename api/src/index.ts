261a821d7dd5367254dedfd22ed1db9b6ccc4ffa
Doruk Eray <doruk@dorkodu.com>
2025-04-07 01:33:15 +0300

    feat(api): drizzle, elysia, betterauth

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,39 +1,9 @@
-import { config } from "@api/config"
-import { express } from "@api/lib/express"
-import { passport } from "@api/lib/passport"
-import { authService } from "@api/namespaces/auth/service"
-import { tokenUtil } from "@api/namespaces/auth/token-util"
-import { appRouter } from "@api/router"
-import { createExpressMiddleware } from "@trpc/server/adapters/express"
+import { Elysia } from 'elysia'
 
-express.use(
-  "/trpc",
-  createExpressMiddleware({
-    router: appRouter,
-    createContext: ({ req, res }) => ({ req, res }),
-  })
+const app = new Elysia()
+  .get('/', () => 'Hello Elysia')
+  .listen(3000)
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 )
-
-express.get(
-  "/oauth/google",
-  passport.authenticate("google", { scope: ["email", "profile"] })
-)
-
-express.get(
-  "/oauth/google/callback",
-  passport.authenticate("google", { session: false, failureRedirect: "/" }),
-  async (req, res) => {
-    const userId = (req.user as any).id as string | undefined
-
-    if (userId) {
-      const session = await authService.createSession(userId)
-      if (session) tokenUtil.setSession(res, session.token, session.expiresAt)
-    }
-
-    res.redirect("/")
-  }
-)
-
-express.listen(config.port, () => {
-  console.log(`Server has started on port ${config.port}`)
-})
