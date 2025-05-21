06fb266fa60966460a91745a4555adb3bcde4544
Doruk Eray <doruk@dorkodu.com>
2025-05-21 11:58:30 +0300

    feat(web): add shadcn theme provider

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,22 +1,22 @@
-import { ErrorComponent, RouterProvider, createRouter } from "@tanstack/react-router"
-import { StrictMode } from "react"
-import ReactDOM from "react-dom/client"
+import { ErrorComponent, RouterProvider, createRouter } from "@tanstack/react-router";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
 
-import * as TanstackQuery from "./lib/tanstack-query/root-provider"
-import reportWebVitals from "./reportWebVitals"
-import { routeTree } from "./routeTree.gen"
+import * as TanstackQuery from "./lib/tanstack-query/root-provider";
+import reportWebVitals from "./reportWebVitals";
+import { routeTree } from "./routeTree.gen";
 
-import "./styles.css"
+import "./styles.css";
 
 declare module "@tanstack/react-router" {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
 
 const router = createRouter({
   routeTree,
-  defaultPendingComponent: () => (<div className="p-2 text-2xl">LOADING...</div>),
+  defaultPendingComponent: () => <div className="p-2 text-2xl">LOADING...</div>,
   defaultErrorComponent: ({ error }) => <ErrorComponent error={error} />,
 
   context: {
@@ -25,22 +25,24 @@ const router = createRouter({
 
   defaultPreload: "intent",
   scrollRestoration: true,
-})
+});
 
-const rootElement = document.getElementById("app")
+const rootElement = document.getElementById("app");
 
 if (rootElement && !rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
+  const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
       <TanstackQuery.Provider>
-        <RouterProvider router={router} />
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <RouterProvider router={router} />
+        </ThemeProvider>
       </TanstackQuery.Provider>
-    </StrictMode>,
-  )
+    </StrictMode>
+  );
 }
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
