31d8c0d8d2e43f7310aaa6bd2aada8b6597b34a6
Doruk Eray <doruk@dorkodu.com>
2025-07-17 15:30:51 +0300

    feat(web): new app layout rewrite

--- web/src/routes/_app.tsx
+++ web/src/routes/_app.tsx
@@ -4,8 +4,51 @@ export const Route = createFileRoute("/_app")({
   component: AppLayout,
 });
 
-function AppLayout() {
+import { SidebarLeft } from "@web/components/sidebar-left";
+import { SidebarRight } from "@web/components/sidebar-right";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+} from "@web/components/ui/breadcrumb";
+import { Separator } from "@web/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@web/components/ui/sidebar";
+
+export default function AppLayout() {
   return (
-    <Outlet />
-  );
+    <main className="flex items-center justify-center">
+      <SidebarProvider>
+        <SidebarLeft />
+        <SidebarInset>
+          <header className="bg-background sticky top-0 flex h-14 shrink-0 items-center gap-2">
+            <div className="flex flex-1 items-center gap-2 px-3">
+              <SidebarTrigger />
+              <Separator
+                orientation="vertical"
+                className="mr-2 data-[orientation=vertical]:h-4"
+              />
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem>
+                    <BreadcrumbPage className="line-clamp-1">
+                      Project Management & Task Tracking
+                    </BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
+          </header>
+          <div className="flex flex-1 flex-col gap-4 p-4">
+            <Outlet />
+          </div>
+        </SidebarInset>
+        <SidebarRight />
+      </SidebarProvider>
+    </main>
+  )
 }
